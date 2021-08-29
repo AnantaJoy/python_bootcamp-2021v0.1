2bc37bf4568d8aadf6916fcdcad3d3f41d545db2
ananta-joy <anantajoy007@gmail.com>
2021-08-29 15:24:07 +0600

    day 5: loops(for, while)

---
+++ Day 5/for.py
@@ -0,0 +1,19 @@
+# for
+
+# 1 to 10 number print
+for i in range(1,11,2):
+    print(i)
+    
+
+# loop in string
+
+username = "Python"
+
+for t in username:
+    print(t)
+    
+#  loop in lists
+lists = ["A", "B", "C", "D", "E", "F", "G"]
+
+for alpha in lists:
+    print(alpha)
---
+++ Day 5/while.py
@@ -0,0 +1,6 @@
+
+initial_val = 0
+
+while initial_val <= 10:
+    print(initial_val)
+    initial_val += 1
